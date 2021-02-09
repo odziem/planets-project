9ca28fcd0fe2ec8702cd1f5fb9d35cf0b93f76eb
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-02-09 10:20:25 -0500

    Read stream no parsing

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const parse = require('csv-parse');
+const fs = require('fs');
+
+const results = [];
+
+fs.createReadStream('kepler_data.csv')
+  .on('data', (data) => {
+    results.push(data);
+  })
+  .on('error', (err) => {
+    console.log(err);
+  })
+  .on('end', () => {
+    console.log(results);
+    console.log('done');
+  });
+// parse();
