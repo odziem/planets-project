00dadfffc1fc7dd25f7e53e51b3aab6352b36c97
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-02-10 22:42:56 -0500

    Habitable planets filter

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const parse = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
 
 fs.createReadStream('kepler_data.csv')
   .pipe(parse({
@@ -9,13 +15,13 @@ fs.createReadStream('kepler_data.csv')
     columns: true,
   }))
   .on('data', (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
   })
   .on('error', (err) => {
     console.log(err);
   })
   .on('end', () => {
-    console.log(results);
-    console.log('done');
+    console.log(`${habitablePlanets.length} habitable planets found!`);
   });
-  
