654a6e6e6085d3d879e53e3480e7feee29736663
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-02-11 01:26:54 -0500

    List planet names

--- index.js
+++ index.js
@@ -23,5 +23,8 @@ fs.createReadStream('kepler_data.csv')
     console.log(err);
   })
   .on('end', () => {
+    console.log(habitablePlanets.map((planet) => {
+      return planet['kepler_name'];
+    }));
     console.log(`${habitablePlanets.length} habitable planets found!`);
   });
